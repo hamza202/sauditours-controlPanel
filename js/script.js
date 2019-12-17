d3a38930f500e31a92546aa715e942e877b2b2fb
hamza202 <k8668975@gmail.com>
2019-12-17 06:09:36 +0200

    17/12

--- js/script.js
+++ js/script.js
@@ -3,4 +3,8 @@ document.write('<script type="text/javascript" src="js/popper.min.js" ></script>
 document.write('<script type="text/javascript" src="js/bootstrap.min.js" ></script>');
 document.write('<script type="text/javascript" src="js/mmenu.js" ></script>');
 document.write('<script type="text/javascript" src="js/sweetalert2.all.min.js" ></script>');
+document.write('<script type="text/javascript" src="js/fileinput.min.js" ></script>');
+document.write('<script type="text/javascript" src="js/locales/ar.js" ></script>');
+document.write('<script type="text/javascript" src="js/fas/theme.js" ></script>');
+document.write('<script type="text/javascript" src="js/explorer-fas/theme.js" ></script>');
 
